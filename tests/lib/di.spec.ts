1ebb513f9b7a50a32d75b278a6d48542bd21b464
Eduardo Barron <barron.luccioni@gmail.com>
2021-08-29 22:17:35 -0500

    test: add resolve depedencies

--- tests/lib/di.spec.ts
+++ tests/lib/di.spec.ts
@@ -3,7 +3,7 @@ import {DependencyContainer, Injectable} from '../../src/lib/internal';
 
 describe('Test DI', () => {
 
-    test('Should return dependencies in order', async () => {
+    test('Should order dependencies', async () => {
         const instance = DependencyContainer.makeContainer(
             [
                 {
@@ -36,6 +36,41 @@ describe('Test DI', () => {
         await instance.execute();
         const nodes = instance.graph.overallOrder();
         expect(nodes).toStrictEqual([ 'b', 'a', 'c', 'd', 'e' ])
+    });
+
+    test('Should resolve dependencies', async () => {
+        const instance = DependencyContainer.makeContainer(
+            [
+                {
+                    bind: 'a',
+                    to: 1
+                },
+                {
+                    bind: 'b',
+                    to: 1
+                },
+                {
+                    bind: 'c',
+                    factory: async (arg1, arg2) => {
+                        return arg1 + arg2;
+                    },
+                    factoryDeps: ['a', 'b']
+                },
+                {
+                    bind: 'd',
+                    factory: async (arg1) => {
+                        return () => arg1;
+                    },
+                    factoryDeps: ['c']
+                },
+            ]
+        );
+        await instance.execute();
+        const container = instance.container;
+        const value = container.get('d') as () => any
+        expect(typeof value).toBe('function');
+        const result = value();
+        expect(result).toBe(2);
     });
 
     test('Should insert metadata in decorator of @Injectable', () => {
