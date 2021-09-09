b1f71af90b424ce901b0bb0844420cf80816b2ec
Eduardo Barron <barron.luccioni@gmail.com>
2021-09-08 23:09:54 -0500

    test: add resolver

--- tests/lib/di.spec.ts
+++ tests/lib/di.spec.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import {DependencyContainer, Inject, Injectable} from '../../src/lib/internal';
 
+jest.setTimeout(1000 * 10);
+
 describe('Test DI', () => {
 
     test('Should order dependencies', async () => {
@@ -108,6 +110,83 @@ describe('Test DI', () => {
         const container = instance.container;
         const value = container.get('c')
         expect(value).toBe(5);
+    });
+
+    test('Should resolve activations', async () => {
+        @Injectable()
+        class ServiceA{
+
+            initial = 1;
+
+            async onActivation(){
+                await new Promise((resolve) => {
+                    setTimeout(() => {
+                        console.log("timeout 3000")
+                        resolve(null);
+                    }, 3000);
+                });
+            }
+
+            far(){
+                return 5;
+            }
+        }
+
+        @Injectable()
+        class ServiceB{
+            constructor(
+                public _service : ServiceA
+            ){}
+
+            async onActivation(){
+                await new Promise((resolve) => {
+                    setTimeout(() => {
+                        console.log("timeout 1000-2")
+                        this._service.initial++;
+                        resolve(null);
+                    }, 1000);
+                });
+            }
+
+            bar(){
+                return this._service.far();
+            }
+        }
+
+        @Injectable()
+        class ServiceC{
+            constructor(
+                private _service : ServiceB
+            ){}
+
+            async onActivation(){
+                await new Promise((resolve) => {
+                    setTimeout(() => {
+                        console.log("timeout 1000-1")
+                        this._service._service.initial++;
+                        resolve(null);
+                    }, 1000);
+                });
+            }
+
+            far(){
+                return this._service._service.initial;
+            }
+        }
+
+        const instance = DependencyContainer.makeContainer(
+            [
+                ServiceB,
+                ServiceA,
+                ServiceC
+            ]
+        );
+        await instance.execute();
+        const container = instance.container;
+        const value = container.get(ServiceC);
+        await instance.resolver.resolve();
+
+        expect(value.far()).toBe(3);
     });
 
     test('Should insert metadata in decorator of @Injectable', () => {
