1530ef4160a8faa5d47a7da0a498e0bcd4fd3b82
Eduardo Barron <barron.luccioni@gmail.com>
2021-09-10 21:43:32 -0500

    feat: clean resolver

--- src/lib/di.ts
+++ src/lib/di.ts
@@ -5,10 +5,9 @@ import {
     optional
 } from "inversify";
 import { nanoid } from 'nanoid';
-import { DepGraph } from 'dependency-graph';
 
 export interface IActivation{
-    onActivation() : Promise<void> | void;
+    onActivation() : Promise<void>;
 }
 
 export type BindType = string | symbol | {new (...args : any[]) : any}
@@ -16,8 +15,6 @@ export type BindType = string | symbol | {new (...args : any[]) : any}
 export type DependencyElementObject = {
     bind: BindType,
     to?: any,
-    factory?: (...args : any[]) => any,
-    factoryDeps?: BindType[],
 }
 
 export type DependencyElement = DependencyElementObject | {new (...args : any[]) : any}
@@ -25,13 +22,7 @@ export type DependencyElement = DependencyElementObject | {new (...args : any[])
 export class DependencyContainer{
 
     private _container : InversifyContainer;
-    private _mapBind : {
-        [key in string]: DependencyElement
-    } = {} as any;
-    private _onlyKeysDependencies : string[] = [];
-    private _graph : DepGraph<any> = new DepGraph();
     private _resolver : ResolverDependency = new ResolverDependency();
-
     constructor(
         private _dependencyList : DependencyElement[]
     ){
@@ -47,122 +38,30 @@ export class DependencyContainer{
     }
 
     async execute(){
-        this.createNodes();
-        this.createDependencies();
         await this.resolveDependencies();
     }
 
-    private getKeyForMap(element : BindType){
-        let key : string;
-        switch(typeof element){
-            case 'string':
-                key = element;
-                break;
-            case 'symbol':
-                key = Symbol.keyFor(element);
-                break;
-            case 'function':
-                /***
-                 * TODO: get metadata id from @injectable
-                 */
-                key = Reflect.getMetadata('proto:id', element);
-                break;
-            default:
-                throw new Error();
-        }
-        return key;
-    }
-
-    private createNodes(){
-        this._dependencyList.forEach((element) => {
-            if(
-                typeof element === 'object' && 
-                    element !== null &&
-                    ('bind' in element)
-            ){
-                const key = this.getKeyForMap(element.bind);
-                this._mapBind[key] = element;
-                if(
-                    element.factoryDeps instanceof Array &&
-                    element.factoryDeps.length > 0
-                ){
-                    this._onlyKeysDependencies.push(key);
-                }
-                this._graph.addNode(key);
-            }
-            if(
-                typeof element === 'function' &&
-                    element.constructor
-            ){
-                const key = this.getKeyForMap(element);
-                this._mapBind[key] = {
-                    bind: element,
-                    to: element
-                };
-                this._graph.addNode(key);
-            }
-        });
-    }
-
-    private createDependencies(){
-        this._onlyKeysDependencies.forEach((element) => {
-            const ref = this._mapBind[element];
-            if(!(typeof ref === 'function')){
-                (ref.factoryDeps ?? []).forEach((item) => {
-                    const keyTo = this.getKeyForMap(item);
-                    this._graph.addDependency(element, keyTo);
-                });
-            }
-        });
-    }
-
     private async resolveDependencies(){
-        const nodes = this.graph.overallOrder();
-        for(const element of nodes){        
-            const ref = this._mapBind[element];
-            if(
-                typeof ref === 'object' && 
-                    ref !== null &&
-                    ('bind' in ref) &&
-                    ('to' in ref)
-            ){
-                if(typeof ref.to === 'function'){
-                    const binding = this._container.bind(ref.bind).to(ref.to);
-                    const hasActivation = hasActivationHandler(ref.to);
+        const nodes = this._dependencyList;
+        for(const element of nodes){
+            const ref = normalizeBind(element);
+            if(typeof ref.to === 'function'){
+                const binding = this._container.bind(ref.bind).to(ref.to);
+                const hasActivation = hasActivationHandler(ref.to);
+                binding.onActivation((_ : any, self : IActivation) => {
+                    const ctx = self;
                     if(hasActivation){
-                        binding.onActivation((_ : any, self : IActivation) => {
-                            this._resolver.addActivation({
-                                handler: self.onActivation,
-                                ctx: self as any as ActivationClassHandler,
-                            });
-                            return self;
+                        this._resolver.addActivation({
+                            handler: ctx.onActivation,
+                            ctx: ctx as any as ActivationClassHandler,
                         });
                     }
-                }else{
-                    this._container.bind(ref.bind).toConstantValue(ref.to);
-                }
-            }else if(
-                typeof ref === 'object' && 
-                    ref !== null &&
-                    ('bind' in ref) &&
-                    ('factory' in ref)
-            ){
-                const hasDeps = ref.factoryDeps instanceof Array &&
-                                    ref.factoryDeps.length > 0;
-                const deps = (hasDeps ? ref.factoryDeps : []).map((_element) => {
-                    const getBind = this._mapBind[this.getKeyForMap(_element)];
-                    return this._container.get(getBind.bind);
-                });
-                const resolve = await ref.factory.apply(null, deps);
-                this._container.bind(ref.bind).toConstantValue(resolve);
+                    return ctx;
+                }); 
             }else{
-                throw new Error();
+                this._container.bind(ref.bind).toConstantValue(ref.to);
             }
         }
-    }
-
-    public get graph(){
-        return this._graph;
     }
 
     public get container(){
@@ -205,7 +104,7 @@ export class ResolverDependency{
 export type ActivationClassHandler = {new (...args : any[]) : IActivation};
 
 export type ResolverDependencyActivation = {
-    handler: () => void | Promise<void>,
+    handler(): Promise<void>,
     ctx: ActivationClassHandler
 }
 
@@ -234,4 +133,14 @@ export function Optional(){
 
 function hasActivationHandler(target : any){
     return target.prototype && target.prototype.onActivation;
+}
+
+function normalizeBind(binding : any) : DependencyElementObject{
+    if(typeof binding === 'function'){
+        return {
+            to: binding,
+            bind: binding
+        }
+    }
+    return binding;
 }
--- tests/lib/di.spec.ts
+++ tests/lib/di.spec.ts
@@ -1,116 +1,10 @@
+import { Container, ContainerModule, interfaces } from 'inversify';
 import 'reflect-metadata';
 import {DependencyContainer, Inject, Injectable} from '../../src/lib/internal';
 
 jest.setTimeout(1000 * 10);
 
 describe('Test DI', () => {
-
-    test('Should order dependencies', async () => {
-        const instance = DependencyContainer.makeContainer(
-            [
-                {
-                    bind: 'a',
-                    to: 1
-                },
-                {
-                    bind: 'd',
-                    factory: async () => {},
-                    factoryDeps: ['c']
-                },
-                {
-                    bind: 'b',
-                    to: 1
-                },
-                {
-                    bind: 'c',
-                    factory: async () => {},
-                    factoryDeps: ['a']
-                },
-                {
-                    bind: 'e',
-                    factory: async () => {
-                        return () => {}
-                    },
-                    factoryDeps: ['d']
-                },
-            ]
-        );
-        await instance.execute();
-        const nodes = instance.graph.overallOrder();
-        expect(nodes).toStrictEqual([ 'b', 'a', 'c', 'd', 'e' ])
-    });
-
-    test('Should resolve dependencies', async () => {
-        const instance = DependencyContainer.makeContainer(
-            [
-                {
-                    bind: 'a',
-                    to: 1
-                },
-                {
-                    bind: 'b',
-                    to: 1
-                },
-                {
-                    bind: 'c',
-                    factory: async (arg1, arg2) => {
-                        return arg1 + arg2;
-                    },
-                    factoryDeps: ['a', 'b']
-                },
-                {
-                    bind: 'd',
-                    factory: async (arg1) => {
-                        return () => arg1;
-                    },
-                    factoryDeps: ['c']
-                },
-            ]
-        );
-        await instance.execute();
-        const container = instance.container;
-        const value = container.get('d') as () => any
-        expect(typeof value).toBe('function');
-        const result = value();
-        expect(result).toBe(2);
-    });
-
-    test('Should resolve nested dependencies', async () => {
-        @Injectable()
-        class ServiceA{
-            far(){
-                return 5;
-            }
-        }
-
-        @Injectable()
-        class ServiceB{
-            constructor(
-                private _service : ServiceA
-            ){}
-
-            bar(){
-                return this._service.far();
-            }
-        }
-        const instance = DependencyContainer.makeContainer(
-            [
-                ServiceB,
-                ServiceA,
-                {
-                    bind: 'c',
-                    factory: async (arg1 : ServiceB) => {
-                        return arg1.bar();
-                    },
-                    factoryDeps: [ServiceB]
-                }
-            ]
-        );
-        await instance.execute();
-        const container = instance.container;
-        const value = container.get('c')
-        expect(value).toBe(5);
-    });
 
     test('Should resolve activations', async () => {
         @Injectable()
@@ -196,5 +90,19 @@ describe('Test DI', () => {
             Reflect.hasMetadata('proto:id', anonClass)
         ).toBe(true);
     });
+
+    test('xxx', () => {
+        let warriors = new ContainerModule((bind: interfaces.Bind, unbind: interfaces.Unbind) => {
+            bind<any>("Ninja").toConstantValue(10);
+        });
+        let warriors2 = new ContainerModule((bind: interfaces.Bind, unbind: interfaces.Unbind) => {
+            bind<any>("Ninja2").toConstantValue(11);
+        });
+        let container = new Container();
+        container.load(...[warriors, warriors2]);
+        const d = container.get('Ninja2');
+        console.log(d)
+
+    })
 
 });
