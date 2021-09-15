0f9765d160e591a3ab4574f2dbf8f934f8c49903
Eduardo Barron <barron.luccioni@gmail.com>
2021-09-14 22:48:02 -0500

    test: add for raw-lambda

---
+++ tests/lib/provider/aws/raw-lambda.spec.ts
@@ -0,0 +1,36 @@
+import {RawLambda} from '../../../../src/lib/provider/aws/raw-lambda';
+
+describe('Provider AWS - Raw Lambda', () => {
+
+    let instance : RawLambda;
+
+    beforeEach(() => {
+        instance = new RawLambda();
+    });
+    
+    test('Should format input for AWS provider in simple way', async () => {
+        const event = {request: null};
+        const context = {function: null};
+        instance.setEvent(event);
+        instance.setContext(context);
+        const out = await instance.beforeEntry();
+        expect(out[0]).toBeDefined();
+        expect(out[0].event).toBeDefined();
+        expect(out[0].event).toBe(event);
+        expect(out[0].context).toBeDefined();
+        expect(out[0].context).toBe(context);
+        expect(out[0].state).toBeDefined();
+    });
+
+    test('Should format output for AWS provider in simple way', async () => {
+        const event = {request: null};
+        const context = {function: null};
+        const payload = {body: null};
+        instance.setEvent(event);
+        instance.setContext(context);
+        const out = await instance.afterEntry(payload);
+        expect(out).toBeDefined();
+        expect(out).toBe(payload);
+    });
+
+});
