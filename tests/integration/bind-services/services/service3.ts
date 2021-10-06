335765b91a6abbe67cc1ad680f2b416361cc03c2
Eduardo Barron <barron.luccioni@gmail.com>
2021-10-06 17:48:34 -0500

    test: fix activation

--- tests/integration/bind-services/services/service3.ts
+++ tests/integration/bind-services/services/service3.ts
@@ -9,9 +9,8 @@ export class Service3 implements IActivation{
     value : number = 0;
 
     async onActivation(): Promise<void> {
-        new Promise((resolve) => {
+        await new Promise((resolve) => {
             setTimeout(() => {
-                console.log("resolve")
                 this.value = 500;
                 resolve(null);
             }, 1000);
