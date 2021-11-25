abf2c4cc14a762d41922e80b8fabbd96981ea432
Eduardo Barron <barron.luccioni@gmail.com>
2021-11-24 22:46:27 -0500

    test: change privders

---
+++ tests/lib/provider/providers.spec.ts
@@ -0,0 +1,37 @@
+/**
+ * @group unit/raw-lambda
+ */
+ import {AwsProvider} from '../../../src/lib/provider/providers';
+
+ describe('Provider AWS - Raw Lambda', () => {
+ 
+     let instance : AwsProvider;
+ 
+     beforeEach(() => {
+         instance = new AwsProvider();
+         instance.setContainer({} as any);
+     });
+     
+     test('Should format input for AWS provider in simple way', async () => {
+         const event = {request: null};
+         const context = {function: null};
+         instance.setArgs([event, context]);
+         const out = await instance.beforeEntry([]);
+         expect(out).toBeDefined();
+         expect(out[0]).toBeDefined();
+         expect(out[0]).toBe(event);
+         expect(out[1]).toBeDefined();
+         expect(out[1]).toBe(context);
+     });
+ 
+     test('Should format output for AWS provider in simple way', async () => {
+         const event = {request: null};
+         const context = {function: null};
+         const payload = {body: null};
+         instance.setArgs([event, context]);
+         const out = await instance.afterEntry(payload, []);
+         expect(out).toBeDefined();
+         expect(out).toBe(payload);
+     });
+ 
+ });
