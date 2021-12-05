106a52ccf55493b1b139915db7718fd0b0bfe29a
Eduardo Barron <barron.luccioni@gmail.com>
2021-12-04 19:05:03 -0500

    test: add middleware behaviour

--- tests/lib/complement/middleware/lib/core.spec.ts
+++ tests/lib/complement/middleware/lib/core.spec.ts
@@ -2,7 +2,7 @@
  * @group unit/middleware/core
  */
 import 'reflect-metadata';
-import { createDecorator, executeMiddleware, getDecorators, MiddlewareObject, MiddlewareOrder, ProviderInfo } from "../../../../../src/lib/internal"
+import { createDecorator, executeMiddleware, getDecorators, IEntryPoint, MiddlewareObject, MiddlewareOrder, MiddlewareParams, ProcessInfo } from "../../../../../src/lib/internal"
 
 describe('Test Middleware', () => {
 
@@ -20,7 +20,12 @@ describe('Test Middleware', () => {
         const args = [event, context];
         const middlewares = [];
         const container = null;
-        const provider = {provider: 'aws'} as ProviderInfo
+        const provider = {
+            provider: 'aws',
+            finish: {
+                flag: false
+            }
+        } as any
         await expect(
             executeMiddleware(args, middlewares, container, provider)
         ).resolves.not.toThrow();
@@ -30,7 +35,12 @@ describe('Test Middleware', () => {
         const event = {};
         const context = {};
         const args = [event, context];
-        const provider = {provider: 'aws'} as ProviderInfo
+        const provider = {
+            provider: 'aws',
+            finish: {
+                flag: false
+            }
+        } as any
         const middlewares : MiddlewareObject[] = [
             {
                 executor: fakeMiddleware,
@@ -76,7 +86,12 @@ describe('Test Middleware', () => {
             isBound: jest.fn().mockReturnValue(true),
             get: jest.fn().mockImplementation(obj => obj)
         };
-        const provider = {provider: 'aws'} as ProviderInfo
+        const provider = {
+            provider: 'aws',
+            finish: {
+                flag: false
+            }
+        } as any
         await expect(
             executeMiddleware(args, middlewares, container as any, provider)
         ).resolves.not.toThrow();
@@ -86,13 +101,90 @@ describe('Test Middleware', () => {
         }, event, context);
     });
 
+    test('Should stop flow when finish is called', async () => {
+        const event = {};
+        const context = {};
+        const params = {};
+        const args = [event, context];
+        const middlewareOne = jest.fn().mockImplementation((params : MiddlewareParams) => {
+            params.finish({});
+        });
+        const middlewareTwo = jest.fn().mockImplementation((params : MiddlewareParams) => {});
+        const middlewares : MiddlewareObject[] = [
+            {
+                executor: middlewareOne,
+                order: MiddlewareOrder.INPUT,
+                params,
+                service: null
+            },
+            {
+                executor: middlewareTwo,
+                order: MiddlewareOrder.INPUT,
+                params,
+                service: null
+            }
+        ];
+        const container = {
+            isBound: jest.fn().mockReturnValue(true),
+            get: jest.fn().mockImplementation(obj => obj)
+        };
+        const provider = {
+            provider: 'aws',
+            finish: {
+                flag: false
+            }
+        } as any
+        await expect(
+            executeMiddleware(args, middlewares, container as any, provider)
+        ).resolves.not.toThrow();
+        expect(middlewareOne).toHaveBeenCalled();
+        expect(middlewareTwo).not.toHaveBeenCalled();
+    });
+
+    test('Should save when decorator is called', async () => {
+        const event = {};
+        const context = {};
+        const params = {};
+        const args = [event, context];
+        const middlewareOne = jest.fn().mockImplementation((params : MiddlewareParams) => {
+            params.setDecoratorValue('mykey', 100);
+        });
+        const middlewares : MiddlewareObject[] = [
+            {
+                executor: middlewareOne,
+                order: MiddlewareOrder.INPUT,
+                params,
+                service: null
+            }
+        ];
+        const container = {
+            isBound: jest.fn().mockReturnValue(true),
+            get: jest.fn().mockImplementation(obj => obj)
+        };
+        const provider = {
+            provider: 'aws',
+            finish: {
+                flag: false
+            },
+            decoratorValues: {},
+            entry: null
+        } as ProcessInfo
+        await expect(
+            executeMiddleware(args, middlewares, container as any, provider)
+        ).resolves.not.toThrow();
+        expect(middlewareOne).toHaveBeenCalled();
+        expect(provider.decoratorValues).toHaveProperty('mykey', 100);
+    });
+
     describe('Decorator Middleware Scope', () => {
 
-        let fn = {
-            prototype: {
-                entry: function(){}
+        let fn = class implements IEntryPoint{
+            
+            entry(...args: any[]): Promise<any> {
+                return;
             }
-        };
+            
+        }
 
         beforeEach(() => {
             const Getting = createDecorator('getting');
@@ -105,6 +197,6 @@ describe('Test Middleware', () => {
             expect(decorators[0]).toBe('getting');
         });
 
-    })
+    });
 
 });
